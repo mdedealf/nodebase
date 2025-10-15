ce11af1049d3abebaf179c8a60eaac096aa10453
mdedealf <dede.alfaruqq@gmail.com>
2025-10-15 16:25:03 +0700

    setup : setting up prisma and connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
 
-const Page = () => {
+const Page = async () => {
+  const users = await prisma.user.findMany();
+
   return (
-    <div className="text-red-500 bg-black h-screen">
+    <div className="text-red-500  h-screen">
       Hello Page
       <Button variant="outline">Click me</Button>
+      {users.map((user) => (
+        <div key={user.id}>
+          {user.email} - {user.name}
+        </div>
+      ))}
     </div>
   );
 };
