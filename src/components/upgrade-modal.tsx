0b54ac22625f8096645c614c9b5837bf63ddbe0a
Muhammad Dede Alfaruq <dede.alfaruqq@gmail.com>
2025-10-20 13:20:20 +0700

    feat : adding CRUD for workflows (#9)

---
+++ src/components/upgrade-modal.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { authClient } from "@/lib/auth-client";
+import { FC } from "react";
+
+interface UpgradeModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const UpgradeModal: FC<UpgradeModalProps> = ({ open, onOpenChange }) => {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Upgrade to Pro</AlertDialogTitle>
+          <AlertDialogDescription>
+            You need an active subscription to perform this action. Upgrade to
+            Pro to unlock all features.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => authClient.checkout({ slug: "pro" })}
+          >
+            Upgrade Now
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,27 +1,9 @@
-import { inngest } from "@/inngest/client";
-import {
-  createTRPCRouter,
-  premiumProcedure,
-  protectedProcedure,
-} from "../init";
-import prisma from "@/lib/db";
+import { createTRPCRouter } from "../init";
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: premiumProcedure.mutation(async () => {
-    await inngest.send({ name: "execute/ai" });
-    return { success: true, message: "Job queued" };
-  }),
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "test/hello.world",
-      data: { email: "dede@gmail.com" },
-    });
-
-    return { success: true, message: "Workflow queued" };
-  }),
+  workflows: workflowsRouter,
 });
+
 // export type definition of API
 export type AppRouter = typeof appRouter;
