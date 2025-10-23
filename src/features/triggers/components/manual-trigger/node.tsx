be3e5a8036c97aae541c6c699a714df72dd1cf66
mdedealf <dede.alfaruqq@gmail.com>
2025-10-23 16:27:35 +0700

    feat : adding editor state

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -1,18 +1,26 @@
 import { NodeProps } from "@xyflow/react";
-import { memo } from "react";
+import { memo, useState } from "react";
 import { BaseTriggerNode } from "../base-trigger.node";
 import { MousePointer2Icon } from "lucide-react";
+import { ManualTriggerDialog } from "./dialog";
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const nodeStatus = "initial";
+
+  const handleOpenSettings = () => setDialogOpen(true);
+
   return (
     <>
+      <ManualTriggerDialog onOpenChange={setDialogOpen} open={dialogOpen} />
       <BaseTriggerNode
         {...props}
         icon={MousePointer2Icon}
         name="When clicking 'Execute workflow'"
-        // status={nodeStatus}
-        // onSettings={handleOpenSettings}
-        // onDoubleClick={handleOpenSettings}
+        status={nodeStatus}
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
